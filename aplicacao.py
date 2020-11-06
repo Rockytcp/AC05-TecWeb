ec6e073709beac137199dfb74abc0f6d8bd3092a
Rockytcp <56940852+Rockytcp@users.noreply.github.com>
2020-11-06 17:39:51 -0300

    2

--- aplicacao.py
+++ aplicacao.py
@@ -3,7 +3,28 @@ from flask import render_template
 
 
 app = Flask(__name__)
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html')
+@app.route("/palmeiras.html")
+def palmeiras():
+    return render_template("palmeiras.html")
+@app.route("/liverpool.html")
+def liverpool():
+    return render_template("liverpool.html")
+@app.route("/milan.html")
+def milan():
+    return render_template("milan.html")
+@app.route("/paysandu.html")
+def paysandu():
+    return render_template("paysandu.html")
+@app.route("/portuguesa.html")
+def portuguesa():
+    return render_template("portuguesa.html")
+@app.route("/chelsea.html")
+def chelsea():
+    return render_template("chelsea.html")
+@app.route("/index.html")
+def resultado():
+    return render_template("index.html")
 app.run()
